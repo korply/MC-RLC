4c7cf074c2091dc22c73bffe2b62e80fae11dfbf
korply <63543600+korply@users.noreply.github.com>
2020-04-12 22:13:30 +0700

    McRLCserver

---
+++ RLCraft+Server+Pack+1.12.2+-+Beta+v2.8.1/ServerStart.bat
@@ -0,0 +1,1 @@
+java -server -Xms6G -Xmx6G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
